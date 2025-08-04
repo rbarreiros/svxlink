99e98e198523371166239fd201eba60829314ec9
Rui Barreiros <rbarreiros@gmail.com>
2025-08-05 00:58:55 +0100

    Forgot the license headers

--- src/svxlink/reflector/MqttHandler.h
+++ src/svxlink/reflector/MqttHandler.h
@@ -1,3 +1,28 @@
+/**
+@file	 MqttHandler.h
+@brief   MQTT handler class
+@author  Rui Barreiros / CR7BPM
+@date	 2025-08-05
+
+\verbatim
+Copyright (C) 2025 Rui Barreiros / CR7BPM
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\endverbatim
+*/
+
 #ifndef MQTT_HANDLER_H
 #define MQTT_HANDLER_H
 
